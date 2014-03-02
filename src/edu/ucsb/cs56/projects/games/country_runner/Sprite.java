61082383abfafa8f703997eacb00124c60a9cab5
Sidney <sdrhoads@gmail.com>
2014-03-02 15:33:40 -0800

    JUnit tests for Sprite class and more methods

--- src/edu/ucsb/cs56/projects/games/country_runner/Sprite.java
+++ src/edu/ucsb/cs56/projects/games/country_runner/Sprite.java
@@ -19,3 +19,67 @@ import javax.imageio.ImageIO;
 
 public class Sprite extends GeneralPathWrapper implements Shape
 {
+	double xPosition;
+	double yPosition;
+	BufferedImage currentImage;
+	ImageManager spriteImageManager; 
+	
+	/** Constuctor for the Sprite
+	*	creates a generic sprite instance at an initial (x,y) position
+	*	and the name of the sprite sheets
+	*/	
+	public Sprite(double x, double y, String sheetName)
+	{
+		//@@@ Stub
+	}
+	/** @return returns the sprite's current x position on JPanel
+	* 	
+	*/
+	public double getX()
+	{
+		//@@@ Stub
+		return -1;
+	}
+	/** @return returns the sprite's current y position on JPanel
+	* 	
+	*/
+	public double getY()
+	{
+		//@@@ Stub
+		return -1;
+	}
+	
+	/** sets the sprite's current x position on JPanel
+	* 	
+	*/
+	public void setX(double newXPosition)
+	{
+		//@@@ Stub
+	}
+	/** sets the sprite's current y position on JPanel
+	* 	
+	*/
+	public void setY(double newYPosition)
+	{
+		//@@@ Stub
+	}
+	
+	/** @return returns the sprite's current Image that is 
+	*	that is ready to be displayed  
+	*/
+	public BufferedImage getCurrentImage()
+	{
+		//@@@ Stub cant test this. 
+		return null;
+	}
+	
+	/** sets the sprite's current Image that is 
+	*	that is ready to be displayed  
+	*/
+	public void setCurrentImage(BufferedImage newImage)
+	{
+		//@@@ Stub cant test this. 
+	}
+	
+	
+}
--- src/edu/ucsb/cs56/projects/games/country_runner/SpriteTest.java
+++ src/edu/ucsb/cs56/projects/games/country_runner/SpriteTest.java
@@ -12,10 +12,42 @@ import static org.junit.Assert.assertEquals;
 public class SpriteTest
 {
 	/** Test for Sprite.getX()
+	@see Sprite.getX()
 	*/
 	@Test
-	public void test_getX()
-	{
-		Sprite piggy = new Sprite(300, 300
+		public void test_getX() {
+		Sprite piggy = new Sprite(300, 400, null);
+		assertEquals(300, piggy.getX());
+	}
+	
+	/** Test for Sprite.getY()
+	@see Sprite.getY()
+	*/
+	@Test 
+		public void test_getY() {
+		Sprite piggy = new Sprite(300, 400, null);
+		assertEquals(300, piggy.getY());
+	}
+	
+	/** Test for Sprite.setX()
+	@see Sprite.setX()
+	*/
+	@Test 
+		public void test_setX() {
+		Sprite piggy = new Sprite(300, 400, null);
+		piggy.setX(350);
+		assertEquals(350, piggy.getX());
+	}
+	/** Test for Sprite.setY()
+	@see Sprite.setY()
+	*/
+	@Test 
+		public void test_setY() {
+		Sprite piggy = new Sprite(300, 400, null);
+		piggy.setY(350);
+		assertEquals(350, piggy.getY());
 	}
 }
+	
+	
+	
