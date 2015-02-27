09a2539b4c277ef092a521be8ee989ce7debfbbe
Sal Olivares <salisonhismac@gmail.com>
2015-02-27 15:50:51 -0800

    fix resolution

--- src/edu/ucsb/cs56/projects/games/country_runner/CountryRunnerJPanel.java
+++ src/edu/ucsb/cs56/projects/games/country_runner/CountryRunnerJPanel.java
@@ -68,7 +68,7 @@ public class CountryRunnerJPanel extends JPanel implements Runnable
         scoreLabel = new JLabel("Score: " + Integer.toString(score));
         scoreLabel.setFont(new Font("Arial",Font.BOLD,24));
         scoreLabel.setForeground(Color.BLACK);
-        scoreLabel.setBounds(475,1,200,100);
+        scoreLabel.setBounds(470,1,200,100);
         add(scoreLabel);
 
 		//This part if ro regestering keyboard keys
