c33f2174e91214cd75842212c906bf38d53fe389
annahlee <annahlee@umail.ucsb.edu>
2017-11-11 17:18:45 -0800

    created Platform.java
    haven't written anything in it yet

---
+++ src/edu/ucsb/cs56/projects/games/country_runner/Platform.java
@@ -0,0 +1,11 @@
+package edu.ucsb.cs56.projects.games.country_runner;
+import java.lang.Math;
+
+/**Draws the Platform object on screen
+ * @author Anna Lee, Ricardo Leon
+ * @version cs56, F17
+ *
+ */
+public class Platform extends Obstacle
+{
+}
