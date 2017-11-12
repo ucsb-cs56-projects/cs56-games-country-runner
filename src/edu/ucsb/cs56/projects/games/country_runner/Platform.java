9eb7df41ab3cb77050c5965a33e6cb7c2fd128ba
annahlee <annahlee@umail.ucsb.edu>
2017-11-11 17:31:27 -0800

    Wrote Platform.java
    copied format for other obstacles but changed speed and occurance

--- src/edu/ucsb/cs56/projects/games/country_runner/Platform.java
+++ src/edu/ucsb/cs56/projects/games/country_runner/Platform.java
@@ -6,6 +6,62 @@ import java.lang.Math;
  * @version cs56, F17
  *
  */
-public class Platform extends Obstacle
-{
+public class Platform extends Obstacle {
+    public Platform(int difficulty) {
+        super(100, 50, -100, 0, "platformSheet");
+        switch (difficulty)
+        {
+            case 1: {
+                speed = 10.0;
+                occurance = 30;
+                break;
+            }
+            case 2: {
+                speed = 11.0;
+                occurance = 30;
+                break;
+            }
+            case 3: {
+                speed = 12.0;
+                occurance = 40;
+                break;
+            }
+        }
+        waiting = true;
+        counter = randomWithRange(occurance, occurance + 100); //change to +10 to test
+        score = 0;
+    }
+
+    /** setDifficulty
+     * set speed and occurance 
+     * according to difficulty
+     */
+    public void setDifficulty(int difficulty) {
+	    switch (difficulty) {
+	        case 1: {
+	            speed = 10.0;
+	            occurance = 30;
+	            break;
+	        }
+	        case 2: {
+	            speed = 11.0;
+	            occurance = 30;
+	            break;
+	        }
+	        case 3: {
+	            speed = 12.0;
+	            occurance = 40;
+	            break;
+	        }
+	    }
+	    waiting = true;
+	    counter = randomWithRange(occurance, occurance + 100);
+    }
+    
+    /** incrementScore increments the score by one
+     */
+    public void incrementScore() {
+	    score++;
+    }
+
 }
